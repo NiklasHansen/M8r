8e435b93e4609a76b62cf4e89fd3c2446a7e67e1
Niklas Hansen <nha@3floor.dk>
2021-04-29 14:33:28 +0200

    Included the sample code, to start off with

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::stream::StreamExt;
+use tokio_socketcan::{CANSocket, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut socket_rx = CANSocket::open("vcan0")?;
+    let socket_tx = CANSocket::open("vcan0")?;
+    while let Some(Ok(frame)) = socket_rx.next().await {
+        socket_tx.write_frame(frame)?.await?;
+    }
+    Ok(())
 }
