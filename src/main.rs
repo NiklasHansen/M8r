4d73130d41221186f8b0d40eb44b0092bef1291c
Niklas Hansen <nha@3floor.dk>
2021-05-03 10:42:33 +0200

    Removed socket tx

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use tokio_socketcan::{CANSocket, Error};
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut socket_rx = CANSocket::open("vcan0")?;
-    let socket_tx = CANSocket::open("vcan0")?;
-    while let Some(Ok(frame)) = socket_rx.next().await {
-        socket_tx.write_frame(frame)?.await?;
+    
+    //socket_rx.set_filter(filters: &[socketcan::CANFilter]);
+    while let Some(Ok(_frame)) = socket_rx.next().await {
+        println!("Ayo");
     }
     Ok(())
 }
