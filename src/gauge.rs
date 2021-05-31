443663bbc562fd11e3920d941c30909765ac4c09
Niklas Hansen <nha@3floor.dk>
2021-05-31 20:17:12 +0200

    Moved gauge to separate file

--- src/gauge.rs
+++ src/gauge.rs
@@ -1,16 +1,96 @@
 pub mod gauge {
 
-    pub struct Dial {
+    use embedded_graphics::{
+        draw_target::DrawTarget,
+        mono_font::{ascii::FONT_10X20, MonoTextStyle},
+        pixelcolor::BinaryColor,
+        prelude::*,
+        primitives::{Arc, Line, PrimitiveStyle, PrimitiveStyleBuilder, StrokeAlignment},
+        text::{Alignment, Baseline, Text, TextStyle, TextStyleBuilder},
+        Drawable,
+    };
+
+    pub struct Dial<'a> {
         pub title: String,
         pub min_value: f32,
         pub max_value: f32,
         pub current_value: f32,
 
-
+        arc_stroke: PrimitiveStyle<BinaryColor>,
+        outline: PrimitiveStyle<BinaryColor>,
+        character_style: MonoTextStyle<'a, BinaryColor>,
+        text_style: TextStyle,
     }
 
-    impl Dial {
+    impl Dial<'_> {
+        pub fn new<'a>(
+            title: String,
+            min_value: f32,
+            max_value: f32,
+            current_value: f32,
+        ) -> Dial<'a> {
+            Dial {
+                title,
+                min_value,
+                max_value,
+                current_value,
 
+                arc_stroke: PrimitiveStyleBuilder::new()
+                    .stroke_color(BinaryColor::On)
+                    .stroke_width(5)
+                    .stroke_alignment(StrokeAlignment::Inside)
+                    .build(),
+                outline: PrimitiveStyleBuilder::new()
+                    .stroke_color(BinaryColor::On)
+                    .stroke_width(1)
+                    .stroke_alignment(StrokeAlignment::Inside)
+                    .build(),
+                character_style: MonoTextStyle::new(&FONT_10X20, BinaryColor::On),
+                text_style: TextStyleBuilder::new()
+                    .baseline(Baseline::Middle)
+                    .alignment(Alignment::Center)
+                    .build(),
+            }
+        }
     }
 
+    impl Drawable for Dial<'_> {
+        type Color = BinaryColor;
+        type Output = ();
+
+        fn draw<D>(&self, target: &mut D) -> Result<Self::Output, <D as DrawTarget>::Error>
+        where
+            D: DrawTarget<Color = BinaryColor>,
+        {
+            let percentage = ((self.current_value - self.min_value) / (self.max_value - self.min_value)) * 100.0;
+            let sweep = percentage * 270.0 / 100.0;
+
+            // Draw an arc with a 5px wide stroke.
+            let arc = Arc::new(Point::new(2, 2), 64 - 4, 270.0.deg(), -sweep.deg());
+            arc.into_styled(self.arc_stroke).draw(target)?;
+
+            Arc::with_center(arc.center(), 64 - 4, 270.0.deg(), -270.0.deg())
+                .into_styled(self.outline)
+                .draw(target)?;
+
+            Arc::with_center(arc.center(), 64 - 12, 270.0.deg(), -270.0.deg())
+                .into_styled(self.outline)
+                .draw(target)?;
+
+            Line::new(Point::new(57, 31), Point::new(61, 31))
+                .into_styled(self.outline)
+                .draw(target)?;
+
+            Line::new(Point::new(2, 31), Point::new(6, 31))
+                .into_styled(self.outline)
+                .draw(target)?;
+
+            // Draw centered text.
+            let text = format!("{:.2}", self.current_value);
+            Text::with_text_style(&text, arc.center(), self.character_style, self.text_style)
+                .draw(target)?;
+
+            Ok(())
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,104 +3,52 @@
 
 //#[tokio::main]
 //async fn main() -> Result<(), Error> {
-    //let mut socket_rx = CANSocket::open("vcan0")?;
+//let mut socket_rx = CANSocket::open("vcan0")?;
 
-    //// Gauges:
-    //// - Oil pressure
-    //// - Oil temp
-    //// - MAP
-    //// - Coolant temp
-    //// - EGT?
-    //// - Ignition retard?
-    //// - Cruise control?
-    //// - Error? Limp mode?
-    //// - Fuel pressure?
-    //// - Fuel composition / ethanol content?
-    //// - Lambda?
-    //// - Knock warn light?
+//// Gauges:
+//// - Oil pressure
+//// - Oil temp
+//// - MAP
+//// - Coolant temp
+//// - EGT?
+//// - Ignition retard?
+//// - Cruise control?
+//// - Error? Limp mode?
+//// - Fuel pressure?
+//// - Fuel composition / ethanol content?
+//// - Lambda?
+//// - Knock warn light?
 
-    ////socket_rx.set_filter(filters: &[socketcan::CANFilter]);
-    //while let Some(Ok(frame)) = socket_rx.next().await {
-        //println!("{}", frame.id());
-    //}
-    //Ok(())
+////socket_rx.set_filter(filters: &[socketcan::CANFilter]);
+//while let Some(Ok(frame)) = socket_rx.next().await {
+//println!("{}", frame.id());
+//}
+//Ok(())
 //}
 
-use embedded_graphics::{
-    mono_font::{ascii::FONT_10X20, MonoTextStyle},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    primitives::{Arc, Line, PrimitiveStyleBuilder, StrokeAlignment},
-    text::{Alignment, Baseline, Text, TextStyleBuilder},
-};
+use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
+use gauge::gauge::Dial;
 use std::{thread, time::Duration};
 
-fn main() -> Result<(), std::convert::Infallible> {
-    // Create a new simulator display with 64x64 pixels.
-    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(256, 64));
+mod gauge;
 
-    // Create styles used by the drawing operations.
-    let arc_stroke = PrimitiveStyleBuilder::new()
-        .stroke_color(BinaryColor::On)
-        .stroke_width(5)
-        .stroke_alignment(StrokeAlignment::Inside)
-        .build();
-    let outline = PrimitiveStyleBuilder::new()
-        .stroke_color(BinaryColor::On)
-        .stroke_width(1)
-        .stroke_alignment(StrokeAlignment::Inside)
-        .build();
-    let character_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
-    let text_style = TextStyleBuilder::new()
-        .baseline(Baseline::Middle)
-        .alignment(Alignment::Center)
-        .build();
+fn main() -> Result<(), std::convert::Infallible> {
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(256, 64));
 
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledBlue)
         .build();
     let mut window = Window::new("Boost", &output_settings);
 
-    // The current progress percentage
-    let mut progress = 78;
+    let mut boost = Dial::new("Boost".to_string(), -1.0, 2.0, 1.2);
 
     'running: loop {
         display.clear(BinaryColor::Off)?;
 
-        let sweep = progress as f32 * 270.0 / 100.0;
-
-        // Draw an arc with a 5px wide stroke.
-        let arc = Arc::new(Point::new(2, 2), 64 - 4, 270.0.deg(), -sweep.deg());
-        arc.into_styled(arc_stroke).draw(&mut display)?;
-
-        Arc::with_center(arc.center(), 64 - 4, 270.0.deg(), -270.0.deg())
-            .into_styled(outline)
-            .draw(&mut display)?;
-
-        Arc::with_center(arc.center(), 64 - 12, 270.0.deg(), -270.0.deg())
-            .into_styled(outline)
-            .draw(&mut display)?;
-
-        Line::new(Point::new(57, 31), Point::new(61, 31))
-            .into_styled(outline)
-            .draw(&mut display)?;
-
-        Line::new(Point::new(2, 31), Point::new(6, 31))
-            .into_styled(outline)
-            .draw(&mut display)?;
-
-        // Draw centered text.
-        let text = format!("{}%", progress);
-        Text::with_text_style(
-            &text,
-            arc.center(),
-            character_style,
-            text_style,
-        )
-        .draw(&mut display)?;
+        boost.draw(&mut display)?;
 
         window.update(&display);
 
@@ -109,6 +57,9 @@ fn main() -> Result<(), std::convert::Infallible> {
         }
         thread::sleep(Duration::from_millis(50));
 
-        progress = (progress + 1) % 101;
+        boost.current_value += 0.05;
+        if boost.current_value > boost.max_value {
+            boost.current_value = boost.min_value;
+        }
     }
 }
