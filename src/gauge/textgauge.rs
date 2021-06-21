b986909eadca0b437a4a41706860e8e03eff8437
Niklas Hansen <nha@3floor.dk>
2021-06-21 16:22:19 +0200

    Implemented text gauge

--- src/gauge/textgauge.rs
+++ src/gauge/textgauge.rs
@@ -1,21 +1,91 @@
-use super::DrawableWrapper;
+use super::{Digits, DrawableWrapper};
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    mono_font::{ascii::FONT_6X9, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::Point,
+    primitives::Rectangle,
+    text::{Alignment, Baseline, Text, TextStyleBuilder},
+    Drawable,
+};
 
 pub struct TextGauge<'a> {
     pub title: &'a str,
     pub unit: &'a str,
     pub value: f32,
+    pub digits: Digits,
 
+    bounding_box: Rectangle,
+    character_style: MonoTextStyle<'a, BinaryColor>,
     drawables: Vec<DrawableWrapper<'a>>,
 }
 
 impl TextGauge<'_> {
-    pub fn new<'a>(title: &'a str, unit: &'a str, value: f32) -> TextGauge<'a> {
+    pub fn new<'a>(
+        title: &'a str,
+        unit: &'a str,
+        value: f32,
+        digits: Digits,
+        bounding_box: Rectangle,
+    ) -> TextGauge<'a> {
+        let text_style = TextStyleBuilder::new()
+            .baseline(Baseline::Middle)
+            .alignment(Alignment::Left)
+            .build();
+        let character_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
+        let mut drawables: Vec<DrawableWrapper<'a>> = Vec::new();
+        let center = bounding_box.center();
+
+        drawables.push(DrawableWrapper::Text(Text::with_text_style(
+            title,
+            Point::new(bounding_box.top_left.x + 2, center.y),
+            character_style,
+            text_style,
+        )));
+
         TextGauge {
             title,
             unit,
             value,
+            digits,
 
-            drawables: Vec::new(),
+            bounding_box,
+            character_style,
+            drawables,
         }
+    }
+}
+
+impl Drawable for TextGauge<'_> {
+    type Color = BinaryColor;
+    type Output = ();
+
+    fn draw<D>(&self, target: &mut D) -> Result<Self::Output, <D as DrawTarget>::Error>
+    where
+        D: DrawTarget<Color = BinaryColor>,
+    {
+        
+        let text_style = TextStyleBuilder::new()
+            .baseline(Baseline::Middle)
+            .alignment(Alignment::Right)
+            .build();
+        let value = match self.digits {
+            Digits::None => format!("{:.0}", self.value),
+            Digits::Single => format!("{:.1}", self.value),
+            Digits::Two => format!("{:.2}", self.value),
+        };
+        Text::with_text_style(
+            &format!("{} {}", value, self.unit),
+            Point::new(self.bounding_box.anchor_point(embedded_graphics::geometry::AnchorPoint::TopRight).x - 2, self.bounding_box.center().y),
+            self.character_style,
+            text_style,
+        ).draw(target)?;
+
+        let drawable_iter = self.drawables.iter();
+        for drawable in drawable_iter {
+            drawable.draw(target)?;
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 //// - Lambda?
 //// - Knock warn light?
 
-use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
+use embedded_graphics::{pixelcolor::BinaryColor, prelude::*, primitives::Rectangle};
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
@@ -33,7 +33,9 @@ fn main() -> Result<(), std::convert::Infallible> {
     let mut boost = Dial::new("Boost", -1.0, 2.0, 1.2, Digits::Two, 0, &[0.0]);
     let /*mut*/ oiltemp = Dial::new("Oil temp", 0.0, 150.0, 70.0, Digits::None, 64, &[80.0]);
     let /*mut*/ oilpres = Dial::new("Oil pres", 0.0, 10.0, 0.0, Digits::Single, 128, &[2.0, 4.0, 6.0, 8.0]);
-
+    let coolant = TextGauge::new("H2O", "C", 85.0, Digits::None, Rectangle::new(Point::new(192, 2), Size::new(64, 10)));
+    let iat = TextGauge::new("IAT", "C", 32.0, Digits::None, Rectangle::new(Point::new(192, 12), Size::new(64, 10)));
+    
     // TODO: Set up filter, to filter out frames not relevant.
     let socket = CANSocket::open("vcan0").unwrap();
     let target_fps = 30;
@@ -46,6 +48,8 @@ fn main() -> Result<(), std::convert::Infallible> {
         boost.draw(&mut display)?;
         oiltemp.draw(&mut display)?;
         oilpres.draw(&mut display)?;
+        coolant.draw(&mut display)?;
+        iat.draw(&mut display)?;
 
         window.update(&display);
 
